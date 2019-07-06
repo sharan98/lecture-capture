f91a613b9fe7303834fd2111de3f2092049390d6
Sharan Rudresh <rudreshsharan98@users.noreply.github.com>
2019-07-06 22:05:48 +0530

    first commit

---
+++ src/layouts/Dashboard/components/index.jsx
@@ -0,0 +1,13 @@
+
+
+export { default as Footer } from './Footer';
+export { default as SidebarStudent } from './Sidebar/SidebarStudent';
+export { default as SidebarAdmin } from './Sidebar/SidebarAdmin';
+export { default as Topbar } from './Topbar';
+
+
+// get the React object from the react module
+
+
+
+// expose the HelloWorld component to other modules
